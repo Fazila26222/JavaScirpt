2c2990dbbf9bd57af8b9d94f4161a53e1b8057cd
Fazila Ahmadi <ahmadi.fa2023@gmail.com>
2026-07-26 16:49:34 +0430

    Add initial HTML structure and JavaScript file for conditional statements

---
+++ Day 9/conditinalStatements.js
@@ -0,0 +1,16 @@
+let age = 20;
+if (age > 18) {
+  console.log("this preson is old", age);
+}
+
+let Age = 16;
+if (Age < 18) {
+  console.log("this preson is yungn", Age);
+}
+
+let user = "Fazila";
+let password = "123";
+
+if (user == "Fazila" && password == "123"){
+    console.log("login")
+}
